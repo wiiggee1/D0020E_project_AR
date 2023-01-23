dcaad9c8a05ed452aae3080a6fc86356c979a5f5
Jenny <jenny.sundstrom@mobilaris.se>
2023-01-23 15:23:52 +0100

    first commit

--- School layout/Assets/OurFolders/Main/Main.cs
+++ School layout/Assets/OurFolders/Main/Main.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+    public Obstacles obstacles;
+    public PosPers posPers;
+    public Timer timer;
+    public boolean QRscanned;
+    public boolean checkForQR;
+    public int obstacleModel;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+    
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    //handler of issue #16 succesfull scan of QR
+    void QRscan(int obsModel) {
+            QRscanned = true;
+            obstacleModel = obsModel;
+    }
+
+    //handler of issue #17
+    void QRfail(int obsModel) {
+                Debug.Log("Not a valid QR code");
+        }
+
+    void initGame() {
+        if (QRscanned == true) {
+        obstacles.placeObstacles(int obstacleModel);
+        posPers.startPosPers();
+        timer.startTimer();
+        }
     }
 }
