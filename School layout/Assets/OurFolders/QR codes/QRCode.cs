8dce5651f6859c701698152b1aebc350a5ac9ab9
karruc-9 <karruc.9@student.ltu.se>
2023-01-24 17:15:55 +0100

    qr code draft

---
+++ School layout/Assets/OurFolders/QR codes/QRCode.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QRCode
+{
+    private int roomId;
+
+    // Constructor with default data that each QR code shall have
+    public QRCode (int roomId) {
+        this.roomId = roomId;
+    }
+}
