4a753826ea3215624f33aa8d6150a434f4a741ae
karruc-9 <karruc.9@student.ltu.se>
2023-01-24 17:20:59 +0100

    qr code representation

--- School layout/Assets/OurFolders/QR codes/QRCode.cs
+++ School layout/Assets/OurFolders/QR codes/QRCode.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class QRCode
 {
     private int roomId;
+    private BitArray qrAsBits;
 
     // Constructor with default data that each QR code shall have
-    public QRCode (int roomId) {
+    public QRCode (int roomId, BitArray qrAsBits) {
         this.roomId = roomId;
+        this.qrAsBits = qrAsBits;
     }
 }
