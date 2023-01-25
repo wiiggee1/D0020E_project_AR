8751b4722a5544260651751c4bd744d53a7a007d
emilwibergh <emil.wibergh@gmail.com>
2023-01-25 08:00:15 +0100

    PosPers Added

--- School layout/Assets/OurFolders/Positioning Class/PosPers.cs
+++ School layout/Assets/OurFolders/Positioning Class/PosPers.cs
@@ -2,17 +2,59 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PosPers : MonoBehaviour
+public class PosPers
 {
-    // Start is called before the first frame update
-    void Start()
+    private float x;
+    private float y;
+    private float z;
+    private float upDown;
+    private float leftRight;
+
+    public PosPers( float x, float y, float z, float upDown, float leftRight ) 
     {
-        
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.upDown = upDown;
+        this.leftRight = leftRight;
     }
 
-    // Update is called once per frame
-    void Update()
+    
+    public void setPosPers( float x, float y, float z, float upDown, float leftRight ) 
     {
-        
+        this.x = x; 
+        this.y = y; 
+        this.z = z; 
+        this.upDown = upDown;
+        this.leftRight = leftRight;
     }
+
+    public ( float x, float y, float z, float upDown, float leftRight ) getPos() 
+    { 
+        return ( x, y, z, upDown, leftRight ); 
+    }
+    /*
+    public void setPos( float x, float y, float z ) 
+    {
+        this.x = x; 
+        this.y = y; 
+        this.z = z; 
+    } 
+
+    public ( float x, float y, float z ) getPos() 
+    { 
+        return ( x, y, z ); 
+    }
+
+    public void setPers( float upDown, float leftRight ) 
+    {
+        this.upDown = upDown;
+        this.leftRight = leftRight;
+    }
+    
+    public ( float upDown, float leftRight ) getPers() 
+    { 
+        return ( upDown, leftRight ); 
+    }
+    */
 }
