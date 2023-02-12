f404781a4fa65bd00b86ac4ef9d5f59544006f67
karruc-9 <karruc.9@student.ltu.se>
2023-02-12 19:37:10 +0100

    scanner without anchorizing

--- School layout/Assets/OurFolders/QR codes/QRCode.cs
+++ School layout/Assets/OurFolders/QR codes/QRCode.cs
@@ -7,10 +7,36 @@ public class QRCode
 {
     // Local variables to store default data for each QR code
     private int roomId;
-    // TODO: add location variables
+    private string decodedText;
+    private float pic_x;
+    private float pic_y;
+    private float pic_z;
+    // Size does not matter as we assume to take bottom left corner
+    // Another assumption: QR code vertically. Straight. Wall?
 
     // Constructor with default data that each QR code shall have
-    public QRCode (int roomId, BitArray qrAsBits) {
+    public QRCode (int roomId, string decodedText, float pic_x, float pic_y, float pic_z) {
         this.roomId = roomId;
+        this.decodedText = decodedText;
+        this.pic_x = pic_x;
+        this.pic_y = pic_y;
+        this.pic_z = pic_z;
+    }
+
+    public string getDecodedText()
+    {
+        return decodedText;
+    }
+    public float getPic_x()
+    {
+        return pic_x;
+    }
+    public float getPic_y()
+    {
+        return pic_y;
+    }
+    public float getPic_z()
+    {
+        return pic_z;
     }
 }
--- School layout/Assets/OurFolders/QR codes/QRScanner.cs
+++ School layout/Assets/OurFolders/QR codes/QRScanner.cs
@@ -1,19 +1,89 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 using ZXing;
 
 public class QRScanner : MonoBehaviour
 {
+
+    // Scanner variables
+    WebCamTexture webcamTexture;
+    string QrCode = string.Empty;
+
+    // Create mini database of QR codes
+    QRCode[] qrDB = new QRCode[5] { new QRCode(1, "one", 1.5f, 2.5f, 1.5f),
+                                    new QRCode(2, "two", 2.5f, 3.5f, 1.5f),
+                                    new QRCode(3, "three", 2.5f, 3.5f, 1.5f),
+                                    new QRCode(4, "four", 2.5f, 3.5f, 1.5f),
+                                    new QRCode(5, "five", 2.5f, 3.5f, 1.5f) };
+
+    // Get current QR code
+    QRCode currentQR;
+
     // Start is called before the first frame update
     void Start()
     {
-        // create database of qr codes, general setup
     }
 
     // Update is called once per frame
     void Update()
     {
         // check input from device's camera and if found qr code, react
+
+        if (Main.checkForQR == true)
+        {
+            this.ScanInit();
+        }
+    }
+
+    void ScanInit()
+    {
+        var renderer = GetComponent<RawImage>();
+        webcamTexture = new WebCamTexture(512, 512);
+        renderer.texture = webcamTexture;
+        StartCoroutine(ScanQR());
+    }
+
+    // Taken from https://github.com/nickdu088/Unity-QR-Scanner/blob/master/Assets/Scripts/QRScanner.cs and modified a bit
+    IEnumerator ScanQR()
+    {
+        IBarcodeReader barCodeReader = new BarcodeReader();
+        webcamTexture.Play();
+        var snap = new Texture2D(webcamTexture.width, webcamTexture.height, TextureFormat.ARGB32, false);
+        while (string.IsNullOrEmpty(QrCode))
+        {
+            try
+            {
+                snap.SetPixels32(webcamTexture.GetPixels32());
+                var Result = barCodeReader.Decode(snap.GetRawTextureData(), webcamTexture.width, webcamTexture.height, RGBLuminanceSource.BitmapFormat.ARGB32);
+                if (Result != null)
+                {
+                    QrCode = Result.Text;
+                    if (!string.IsNullOrEmpty(QrCode))
+                    {
+                        foreach (QRCode qrElem in qrDB)
+                        {
+                            if (qrElem.getDecodedText().Equals(QrCode))
+                            {
+                                this.Anchorize();
+                                goto OUTOFWHILE;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { Debug.LogWarning(ex.Message); }
+            yield return null;
+        }
+        OUTOFWHILE:
+        webcamTexture.Stop();
+    }
+
+    void Anchorize()
+    {
+
     }
 }
