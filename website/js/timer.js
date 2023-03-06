a73e25003a4d01de8d142fcff3a4eeb94cf0f069
Jenny <jenny.sundstrom@mobilaris.se>
2023-03-06 15:15:23 +0100

    minus 10 seconds on certain timestamp implemented

--- website/js/timer.js
+++ website/js/timer.js
@@ -1,26 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
-const startBtn = document.getElementById('startbtn');
-const timerElement = document.getElementById('timer');
-const maxTime = 18000; // 3 minutes in deciseconds
-let timeRemaining = maxTime;
-let intervalID;
+  const startBtn = document.getElementById('startbtn');
+  const timerElement = document.getElementById('timer');
+  const maxTime = 180000; // 3 minutes in miliseconds 
+  let timeRemaining = maxTime;
+  let intervalID;
+  const hit_timestamps = [60000, 120000]; // 1 minute and 2 minutes in milliseconds
 
-if (startBtn) {
-    startBtn.addEventListener('click', () => {
+  const startTimer = (maxTime) => { 
+    clearInterval(intervalID);
+    timeRemaining = maxTime;
+
     intervalID = setInterval(() => {
-    timeRemaining -= 10;
-
-    const minutes = Math.floor(timeRemaining / 6000);
-    const seconds = Math.floor((timeRemaining % 6000) / 100);
-    const deciseconds = timeRemaining % 100;
-
-    const timeString = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}:${String(deciseconds).padStart(2, '0')}`;
-    timerElement.innerText = timeString;
-
-    if (timeRemaining <= 0) {
-      clearInterval(intervalID);
+      if (hit_timestamps.includes(timeRemaining)) {
+        timeRemaining -= 10000; // subtract 10 seconds
+      } else {
+        timeRemaining -= 10; // subtract 10 centiseconds
       }
-    }, 100);
+
+      const minutes = Math.floor(timeRemaining / 60000);
+      const seconds = Math.floor((timeRemaining % 60000) / 1000);
+      const centiseconds = Math.floor((timeRemaining % 1000) / 10);
+
+      const timeString = `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}:${String(centiseconds).padStart(2, '0')}`;
+      timerElement.innerText = timeString;
+
+      if (timeRemaining <= 0) {
+        clearInterval(intervalID);
+      }
+    }, 10);
+  };
+
+  startBtn.addEventListener('click', () => {
+    startTimer(maxTime);
   });
-}
 });
