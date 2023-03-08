6f2444a63f10d2a6cebf1666fcfdafdcca08a299
Viktor Smedberg <95021602+wiiggee1@users.noreply.github.com>
2023-03-08 15:39:45 +0100

    Update DataPopupCanvas.cs

--- School layout/Assets/OurFolders/Algorithm/DataPopupCanvas.cs
+++ School layout/Assets/OurFolders/Algorithm/DataPopupCanvas.cs
@@ -1,13 +1,16 @@
+using MySql.Data.MySqlClient;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Net;
+using System.Text;
 using System.Xml;
+using UnityEditor.MemoryProfiler;
+using UnityEditor.PackageManager.Requests;
 using UnityEngine;
 using UnityEngine.Networking;
 using UnityEngine.UI;
-using static UnityEngine.EventSystems.EventTrigger;
-using static UnityEngine.XR.ARSubsystems.XRCpuImage;
+
 
 public class DataPopupCanvas : MonoBehaviour
 {
@@ -15,6 +18,9 @@ public class DataPopupCanvas : MonoBehaviour
     public Button buttonDisplay;
     public Text popupDisplayText;
     public string jsonDataToSend;
+
+    private MySqlConnection connection;
+    private string connectionString = "server=130.240.202.127;user=root;database=d0020e;port=3306;password=456;";
 
     // Start is called before the first frame update
     void Start()
@@ -52,32 +58,45 @@ public class DataPopupCanvas : MonoBehaviour
 
     public void OnUploadButton()
     {
-        StartCoroutine(SqlWebrequest(Algorithm.requestData));
+        InsertPositionData(Algorithm.requestDataDictionary);
+        //StartCoroutine(SqlWebrequest(Algorithm.requestData));
         dataPopupCanvas.gameObject.SetActive(value: true);
 
     }
-    public IEnumerator SqlWebrequest(string inputDataToSend)
+    public IEnumerator SqlWebrequest(string dataRequest)
     {
-        string jsonStringData = inputDataToSend;
+        string jsonStringData = dataRequest;
         var dataDict = Algorithm.requestDataDictionary;
 
         string server_url = "http://130.240.202.127/server_repo/gameSqlHandler.php";
+
+        var request = new UnityWebRequest(server_url, "POST");
+        byte[] bodyRaw = Encoding.UTF8.GetBytes(jsonStringData);
+        request.uploadHandler = new UploadHandlerRaw(bodyRaw);
+        request.downloadHandler = new DownloadHandlerBuffer();
+        request.SetRequestHeader("Content-Type", "application/json");
+        yield return request.SendWebRequest();
+
+        /*
         WWWForm form = new WWWForm();
+
+        form.AddField("data", jsonStringData);
+
         foreach (KeyValuePair<string, float[]> posData in dataDict)
         {
             string key = posData.Key;
             float[] values = posData.Value;
             for (int i = 0; i < values.Length; i++)
             {
-                form.AddField(key + "[]", values[i].ToString());
+                form.AddField(key +"[]", values[i].ToString());
             }
         }
-        //form.AddField("data", jsonStringData);
-        UnityWebRequest www = UnityWebRequest.Post(server_url, form);
 
-        yield return www.SendWebRequest();
+        UnityWebRequest www = UnityWebRequest.Put(server_url, form);
 
-        if (www.result == UnityWebRequest.Result.Success)
+        yield return www.SendWebRequest(); */
+
+        if (request.result == UnityWebRequest.Result.Success)
         {
             if (Algorithm.requestData == "")
             {
@@ -86,15 +105,54 @@ public class DataPopupCanvas : MonoBehaviour
             else
             {
                 SetPopupText("Data sent successfully!");
-                StopAllCoroutines();
+                //StopAllCoroutines();
             }
         }
         else
         {
-            SetPopupText("Error sending data: " + www.error);
+            SetPopupText("Error sending data: " + request.error);
         }
     }
 
+    private void InsertPositionData(Dictionary<string, float[]> positionData)
+    {
+        try
+        {
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+            MySqlCommand command = connection.CreateCommand();
+
+            // Build the query string
+            string query = "INSERT INTO position_data (position_x, position_y, position_z, vertical_position, horizontal_position, timestamp_frame) VALUES ";
+            query += "(@position_x, @position_y, @position_z, @vertical_position, @horizontal_position, @timestamp_frame)";
+
+            int arrayLength = positionData["position_x"].Length;
+            for (int i = 0; i < arrayLength; i++)
+            {
+                // Add the parameters to the command
+                command.Parameters.AddWithValue("@position_x", positionData["position_x"][i]);
+                command.Parameters.AddWithValue("@position_y", positionData["position_y"][i]);
+                command.Parameters.AddWithValue("@position_z", positionData["position_z"][i]);
+                command.Parameters.AddWithValue("@vertical_position", positionData["vertical_position"][i]);
+                command.Parameters.AddWithValue("@horizontal_position", positionData["horizontal_position"][i]);
+                command.Parameters.AddWithValue("@timestamp_frame", positionData["timestamp_frame"][i]);
+
+                // Execute the query
+                command.CommandText = query;
+                command.ExecuteNonQuery();
+
+                // Clear the parameters for the next iteration
+                command.Parameters.Clear();
+            }
+            connection.Close();
+            SetPopupText("Query was successfully executed!");
+        }
+        catch (MySqlException ex)
+        {
+            SetPopupText("Error sending data: " + ex.Message);
+        }
+           
+    }
 
 }
 
